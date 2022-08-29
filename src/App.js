814e7eaf492c5027e6d2f756fa390ef282598f63
Tran Duoc <duoctran2527@gmail.com>
2022-08-29 12:08:02 +0700

    event reactjs

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
 import logo from './logo.svg';
+import Nav from './views/Nav'
 import './App.css';
 
 function App() {
+  const handleClick = () => {
+    console.log("click is working");
+  }
+
   return (
     <div className="App">
+        <Nav/>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <h1>hello react Hooks with TranDuoc</h1>
-        
+        <input type="text" />
+          <button type="button" onClick={()=> handleClick()}>click me</button>
       </header>
     </div>
   );
